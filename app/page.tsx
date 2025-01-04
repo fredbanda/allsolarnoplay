866e70537b335c94c81b01daac0fb797451d6d17
Fred Banda <97950605+fredbanda@users.noreply.github.com>
2025-01-04 16:06:47 +0200

    checks

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-      <div className="text-center text-3xl text-sky-700 mt-28 text-bold">All Solar No Play</div>
+    <>
+      <h1 className="text-center text-3xl text-sky-700 mt-28 text-bold">All Solar No Play</h1>
+
+      <p className="text-red-700 text-center text-2xl"> We are moving in the food chanin</p>
+      </>
   );
 }
